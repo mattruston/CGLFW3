27675cefe155ad7a9e42e719fbaa477ee5af4745
Matt Ruston <matt@rustonmail.com>
2024-08-03 21:02:59 -0500

    Updated package file to use system library

--- Package.swift
+++ Package.swift
@@ -3,5 +3,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "CGLFW3"
+    name: "CGLFW3",
+    products: [
+        .library(name: "CGLFW3", targets: ["CGLFW3"])
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CGLFW3",
+            providers: [
+                .brew(["glfw3"])
+            ]
+        )
+    ]
 )
